10e1b0a7c1b585053bb9bbdbe53f9970b3472ee2
Mykhaylo Shumko <mykhaylo.shumko@nasa.gov>
2020-11-26 20:17:50 -0800

    Working on the model achitechture. Not there yet.

--- microburst_ann/ann/single_microburst_ann.py
+++ microburst_ann/ann/single_microburst_ann.py
@@ -1,24 +1,53 @@
+"""
+This model catagorizes single peaked microbursts that were first 
+identified using the O'Brien et al.'s, 2003 burst parameter.
+"""
+import pathlib
+
 import tensorflow as tf
 import pandas as pd
+import numpy as np
+
+import microburst_ann.config as config
+
+# Load the data
+train_path = pathlib.Path(config.PROJECT_DIR, 'data', 'train.csv')
+test_path = pathlib.Path(config.PROJECT_DIR, 'data', 'test.csv')
+
+train_df = pd.read_csv(train_path, index_col=0)
+test_df = pd.read_csv(test_path, index_col=0)
+
+train_df.dropna(inplace=True)
+test_df.dropna(inplace=True)
+
+train_labels = train_df.pop('label')
+test_labels = test_df.pop('label')
+
+train_dataset = tf.data.Dataset.from_tensor_slices(
+    (train_df.to_numpy(), train_labels.to_numpy())
+    )
+test_dataset = tf.data.Dataset.from_tensor_slices(
+    (test_df.to_numpy(), test_labels.to_numpy())
+    )
+shuffled_train_dataset = train_dataset.shuffle(train_df.shape[0]).batch(1)
 
 
-# Load and normalize the data
-
-
-# Model
+# # Model
 model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(50)),
-  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Flatten(input_shape=(50,)),
+  tf.keras.layers.Dense(10, activation='relu'),
   tf.keras.layers.Dropout(0.2),
   tf.keras.layers.Dense(1),
+  tf.keras.layers.Softmax(),
 ])
-tf.keras.losses
 
 model.compile(optimizer='adam',
-            loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+            loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
             metrics=['accuracy'])
 print(model.summary())
 
-model.fit(x_train, y_train, epochs=5)
+print(model(train_df.iloc[0].to_numpy().reshape((1, 50))))
 
-print(model.evaluate(x_test,  y_test, verbose=2))
+model.fit(shuffled_train_dataset, epochs=3)
+
+print(model.evaluate(test_df.to_numpy(), test_labels, verbose=2))
