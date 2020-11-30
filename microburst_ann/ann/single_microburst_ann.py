4926e3aa14c768bf799256cad5b5c0ad11e0dc52
Mykhaylo Shumko <mykhaylo.shumko@nasa.gov>
2020-11-29 21:25:21 -0800

    Playing around with the ANN paramaters. More neurons, the better the accuracy!

--- microburst_ann/ann/single_microburst_ann.py
+++ microburst_ann/ann/single_microburst_ann.py
@@ -36,17 +36,18 @@ shuffled_train_dataset = train_dataset.shuffle(train_df.shape[0]).batch(1)
 # # Model
 model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(input_shape=(50,)),
+  tf.keras.layers.Dense(25, activation='relu'),
   tf.keras.layers.Dense(10, activation='relu'),
   tf.keras.layers.Dropout(0.2),
   tf.keras.layers.Dense(1, activation='sigmoid'),
 ])
 
-model.compile(optimizer='sgd',
+model.compile(optimizer='adam',
             loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
             metrics=['accuracy'])
 print(model.summary())
 
-print(model(train_df.iloc[0].to_numpy().reshape((1, 50))))
+# print(model(train_df.iloc[0].to_numpy().reshape((1, 50))))
 
 history = model.fit(shuffled_train_dataset, 
                     validation_data=(test_df.to_numpy(), test_labels), 
@@ -56,11 +57,11 @@ history = model.fit(shuffled_train_dataset,
 
 # print(model.evaluate(test_df.to_numpy(), test_labels, verbose=2))
 
-n_correct=0
-for i in range(test_df.shape[0]):
-    if round(model(test_df.iloc[i].to_numpy().reshape((1, 50))).numpy()[0][0]) == test_labels.iloc[i]:
-        n_correct += 1
-print(f'n_correct={n_correct}, n_correct/n_total={n_correct/test_df.shape[0]}')
+# n_correct=0
+# for i in range(test_df.shape[0]):
+#     if round(model(test_df.iloc[i].to_numpy().reshape((1, 50))).numpy()[0][0]) == test_labels.iloc[i]:
+#         n_correct += 1
+# print(f'n_correct={n_correct}, n_correct/n_total={n_correct/test_df.shape[0]}')
 
 pd.DataFrame(history.history).plot(figsize=(8, 5))
 plt.grid(True)
