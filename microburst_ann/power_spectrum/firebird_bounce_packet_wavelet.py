913fb69147254a91dbc9aa4eb68e0297ad773e75
Mykhaylo <msshumko@gmail.com>
2021-01-07 18:47:45 -0800

    Working on the wavelet test script. It seems to work but there may be something wrong with the code.
    It looks like the wavelet can pick out the periodicity, but
    not the decay in the peaks.

--- microburst_ann/power_spectrum/firebird_bounce_packet_wavelet.py
+++ microburst_ann/power_spectrum/firebird_bounce_packet_wavelet.py
@@ -27,7 +27,7 @@ noise: bool
     Toggle if noise should be added.
 """
 import pathlib
-from datetime import date
+from datetime import date, datetime
 
 import numpy as np
 import pandas as pd
@@ -42,6 +42,9 @@ import make_bounce_packet
 ### Parameters ###
 detrend=True
 day = date(2015, 2, 2)
+time_range = [datetime(2015, 2, 2, 6, 12, 0), 
+            datetime(2015, 2, 2, 6, 13, 0)]
+filter_time_range = True
 sc_id = 4
 
 ### Load the FIREBIRD-II microburst counts ###
@@ -54,30 +57,48 @@ data_path = data_paths[0]
 d = spacepy.datamodel.readJSONheadedASCII(str(data_path))
 d['Time'] = pd.to_datetime(d['Time'])
 cadence = float(d.attrs['CADENCE'])
+counts = d['Col_counts'][:, 0]
 
 if detrend:
-    y = pd.DataFrame(data=d['Col_counts'][:, 0])
-    background = y.rolling(int(5/cadence)).quantile(0.50)
-    y -= background
+    counts = pd.DataFrame(data=counts)
+    background = counts.rolling(int(1/cadence)).quantile(0.50)
+    counts -= background
+    counts[np.isnan(counts)] = 0
+    counts = counts.to_numpy().flatten()
+
+if filter_time_range:
+    idt = np.where((d['Time'] > time_range[0]) & (d['Time'] < time_range[1]))[0]
+    counts = counts[idt]
+    roll_n = 10
+    counts = pd.DataFrame(data=counts).rolling(roll_n).mean()
+    counts = np.roll(counts, -roll_n//2).flatten()
+    counts[np.isnan(counts)] = 0
+    time = d['Time'][idt]
+
+    d['Alt'] = d['Alt'][idt]
+    d['Lat'] = d['Lat'][idt]
+    d['Lon'] = d['Lon'][idt]
+
+    if 'background' in locals():
+        background = background.iloc[idt]
 
 # ### Calculate the wavelet power spectrum ###
-w = wavelet_analysis.WaveletDetector(d['Col_counts'][:, 0], d['Time'], 
+w = wavelet_analysis.WaveletDetector(counts, time, 
     cadence, siglvl=0.95,
-    mother='MORLET', dj=0.125, j1=50,
+    mother='MORLET', dj=0.125, j1=60,
     wavelet_param=4*np.pi)
 w.waveletTransform()
 
 # Calculate the bounce period.
-n_downsample=100
+n_downsample=1
 m = IRBEM.MagFields(kext='OPQ77')
-downsampled_t = d['Time'][::n_downsample]
+downsampled_t = time[::n_downsample]
 tb = -1*np.ones(downsampled_t.shape[0], dtype=float)
-z = zip(downsampled_t, d['Alt'][::n_downsample], 
-        d['Lat'][::n_downsample], d['Lon'][::n_downsample])
+z = zip(downsampled_t, d['Alt'], d['Lat'], d['Lon'])
 
-for i, (time, alt, lat, lon) in enumerate(z):
+for i, (t, alt, lat, lon) in enumerate(z):
     try:
-        tb[i] = m.bounce_period({'time':time, 'x1':alt, 'x2':lat, 'x3':lon}, None, 200)
+        tb[i] = m.bounce_period({'time':t, 'x1':alt, 'x2':lat, 'x3':lon}, None, 200)
     except ValueError as err:
         if (('Mirror point below the ground!' in str(err)) or 
             ('This is an open field line!' in str(err))):
@@ -85,14 +106,19 @@ for i, (time, alt, lat, lon) in enumerate(z):
         else:
             raise
 
+
 # ### Make the plot ###
-fig, ax = plt.subplots(2, 1, figsize=(6,8), sharex=True)
-ax[0].plot(d['Time'], d['Col_counts'][:, 0], 'k')
+if detrend:
+    n_panels = 3
+else:
+    n_panels = 2
+fig, ax = plt.subplots(n_panels, 1, figsize=(6,8), sharex=True)
+ax[0].plot(time, counts, 'k')
 
 if detrend:
-    ax[0].plot(d['Time'], background, 'r')
+    ax[0].plot(time, background, 'r')
 
-w.plotPower(ax=ax[1])
-ax[1].plot(downsampled_t, tb, 'r')
+w.plotPower(ax=ax[-1])
+ax[-1].plot(downsampled_t, tb, 'r')
 # ax[1].axhline(peak_period, c='k', zorder=10)
 plt.show()
